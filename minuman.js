ef7e853f662101e2d077195467431fd574f13e4e
rifqydev <156778697+rydevs29@users.noreply.github.com>
2025-11-04 17:46:10 +0700

    Add Indonesian beverage database with nutritional info
    This file contains a comprehensive database of Indonesian beverages, including their nutritional information such as calories, carbohydrates, protein, and fat content.

---
+++ minuman.js
@@ -0,0 +1,96 @@
+// minuman.js → 80 Minuman Indonesia (Data Resmi, Siap Scan!)
+window.minumanDB = {
+  // TEH & VARIAN
+  "air putih": { name: "Air Putih", cal: 0, carb: 0, prot: 0, fat: 0 },
+  "teh tawar": { name: "Teh Tawar", cal: 2, carb: 0, prot: 0, fat: 0 },
+  "teh manis": { name: "Teh Manis", cal: 80, carb: 20, prot: 0, fat: 0 },
+  "es teh": { name: "Es Teh", cal: 80, carb: 20, prot: 0, fat: 0 },
+  "teh hangat": { name: "Teh Hangat", cal: 2, carb: 0, prot: 0, fat: 0 },
+  "teh tarik": { name: "Teh Tarik", cal: 120, carb: 18, prot: 2, fat: 5 },
+  "teh susu": { name: "Teh Susu", cal: 100, carb: 15, prot: 2, fat: 3 },
+  "teh melati": { name: "Teh Melati", cal: 5, carb: 1, prot: 0, fat: 0 },
+  "teh hijau": { name: "Teh Hijau", cal: 3, carb: 0, prot: 0, fat: 0 },
+  "teh oolong": { name: "Teh Oolong", cal: 4, carb: 0, prot: 0, fat: 0 },
+
+  // KOPI & VARIAN
+  "kopi hitam": { name: "Kopi Hitam", cal: 2, carb: 0, prot: 0, fat: 0 },
+  "kopi tubruk": { name: "Kopi Tubruk", cal: 5, carb: 1, prot: 0, fat: 0 },
+  "kopi susu": { name: "Kopi Susu", cal: 120, carb: 15, prot: 3, fat: 5 },
+  "es kopi susu": { name: "Es Kopi Susu", cal: 180, carb: 25, prot: 4, fat: 7 },
+  "kopi gula aren": { name: "Kopi Gula Aren", cal: 140, carb: 20, prot: 2, fat: 5 },
+  "kopi luwak": { name: "Kopi Luwak", cal: 10, carb: 2, prot: 0, fat: 0 },
+  "kopi aceh": { name: "Kopi Aceh", cal: 8, carb: 1, prot: 0, fat: 0 },
+  "kopi toraja": { name: "Kopi Toraja", cal: 7, carb: 1, prot: 0, fat: 0 },
+  "kopi gayo": { name: "Kopi Gayo", cal: 6, carb: 1, prot: 0, fat: 0 },
+  "kopi vietnam": { name: "Kopi Vietnam", cal: 100, carb: 12, prot: 2, fat: 5 },
+
+  // JUS & MINUMAN SEGAR
+  "jus jeruk": { name: "Jus Jeruk", cal: 110, carb: 25, prot: 2, fat: 0 },
+  "jus mangga": { name: "Jus Mangga", cal: 140, carb: 35, prot: 1, fat: 0 },
+  "jus alpukat": { name: "Jus Alpukat", cal: 250, carb: 30, prot: 3, fat: 15 },
+  "jus tomat": { name: "Jus Tomat", cal: 40, carb: 10, prot: 2, fat: 0 },
+  "jus apel": { name: "Jus Apel", cal: 120, carb: 30, prot: 0, fat: 0 },
+  "jus nanas": { name: "Jus Nanas", cal: 130, carb: 32, prot: 1, fat: 0 },
+  "jus semangka": { name: "Jus Semangka", cal: 80, carb: 20, prot: 1, fat: 0 },
+  "jus melon": { name: "Jus Melon", cal: 90, carb: 22, prot: 1, fat: 0 },
+  "jus sirsak": { name: "Jus Sirsak", cal: 150, carb: 35, prot: 2, fat: 0 },
+  "jus markisa": { name: "Jus Markisa", cal: 130, carb: 32, prot: 1, fat: 0 },
+
+  // ES CAMPUR & TRADISIONAL
+  "es teler": { name: "Es Teler", cal: 350, carb: 70, prot: 3, fat: 10 },
+  "es campur": { name: "Es Campur", cal: 320, carb: 65, prot: 2, fat: 8 },
+  "es cendol": { name: "Es Cendol", cal: 280, carb: 55, prot: 3, fat: 6 },
+  "es doger": { name: "Es Doger", cal: 300, carb: 60, prot: 2, fat: 7 },
+  "es oyen": { name: "Es Oyen", cal: 300, carb: 65, prot: 2, fat: 6 },
+  "es podeng": { name: "Es Podeng", cal: 280, carb: 60, prot: 2, fat: 5 },
+  "es goyobod": { name: "Es Goyobod", cal: 260, carb: 55, prot: 2, fat: 4 },
+  "es shanghai": { name: "Es Shanghai", cal: 300, carb: 65, prot: 2, fat: 6 },
+  "es pisang ijo": { name: "Es Pisang Ijo", cal: 350, carb: 70, prot: 4, fat: 8 },
+  "es pallu butung": { name: "Es Pallu Butung", cal: 320, carb: 65, prot: 3, fat: 7 },
+
+  // MINUMAN KHAS DAERAH
+  "bajigur": { name: "Bajigur", cal: 180, carb: 35, prot: 2, fat: 5 },
+  "bandrek": { name: "Bandrek", cal: 150, carb: 30, prot: 1, fat: 4 },
+  "wedang jahe": { name: "Wedang Jahe", cal: 80, carb: 20, prot: 0, fat: 0 },
+  "sekoteng": { name: "Sekoteng", cal: 200, carb: 45, prot: 2, fat: 3 },
+  "wedang ronde": { name: "Wedang Ronde", cal: 250, carb: 50, prot: 3, fat: 5 },
+  "bir pletok": { name: "Bir Pletok", cal: 80, carb: 20, prot: 0, fat: 0 },
+  "es kacang merah": { name: "Es Kacang Merah", cal: 320, carb: 70, prot: 5, fat: 3 },
+  "es kacang hijau": { name: "Es Kacang Hijau", cal: 280, carb: 60, prot: 6, fat: 2 },
+  "es cincau": { name: "Es Cincau", cal: 180, carb: 40, prot: 1, fat: 2 },
+  "es dawet": { name: "Es Dawet", cal: 250, carb: 55, prot: 2, fat: 4 },
+
+  // MINUMAN SEGAR LAINNYA
+  "es kelapa": { name: "Es Kelapa", cal: 120, carb: 30, prot: 1, fat: 0 },
+  "es kelapa muda": { name: "Es Kelapa Muda", cal: 100, carb: 25, prot: 1, fat: 0 },
+  "es buah": { name: "Es Buah", cal: 250, carb: 60, prot: 2, fat: 1 },
+  "es timun serut": { name: "Es Timun Serut", cal: 80, carb: 20, prot: 1, fat: 0 },
+  "es blewah": { name: "Es Blewah", cal: 120, carb: 30, prot: 1, fat: 0 },
+  "es jeruk nipis": { name: "Es Jeruk Nipis", cal: 60, carb: 15, prot: 0, fat: 0 },
+  "es soda gembira": { name: "Es Soda Gembira", cal: 200, carb: 50, prot: 0, fat: 0 },
+  "es kopyor": { name: "Es Kopyor", cal: 220, carb: 50, prot: 1, fat: 3 },
+  "es tape": { name: "Es Tape", cal: 220, carb: 50, prot: 2, fat: 3 },
+  "es sarang burung": { name: "Es Sarang Burung", cal: 280, carb: 60, prot: 2, fat: 5 },
+
+  // SUSU & MINUMAN MODERN
+  "susu sapi": { name: "Susu Sapi", cal: 150, carb: 12, prot: 8, fat: 8 },
+  "susu kental manis": { name: "SKM", cal: 130, carb: 22, prot: 2, fat: 3 },
+  "susu almond": { name: "Susu Almond", cal: 60, carb: 3, prot: 2, fat: 5 },
+  "susu kedelai": { name: "Susu Kedelai", cal: 80, carb: 8, prot: 7, fat: 4 },
+  "yogurt plain": { name: "Yogurt Plain", cal: 100, carb: 12, prot: 5, fat: 3 },
+  "yogurt buah": { name: "Yogurt Buah", cal: 150, carb: 25, prot: 5, fat: 3 },
+  "smoothie pisang": { name: "Smoothie Pisang", cal: 200, carb: 40, prot: 5, fat: 3 },
+  "milkshake cokelat": { name: "Milkshake Cokelat", cal: 350, carb: 50, prot: 8, fat: 12 },
+
+  // MINUMAN KEMASAN (POPULER)
+  "teh botol": { name: "Teh Botol", cal: 90, carb: 22, prot: 0, fat: 0 },
+  "teh kotak": { name: "Teh Kotak", cal: 85, carb: 21, prot: 0, fat: 0 },
+  "aqua": { name: "Aqua", cal: 0, carb: 0, prot: 0, fat: 0 },
+  "le minerale": { name: "Le Minerale", cal: 0, carb: 0, prot: 0, fat: 0 },
+  "pocari sweat": { name: "Pocari Sweat", cal: 60, carb: 15, prot: 0, fat: 0 },
+  "mizone": { name: "Mizone", cal: 70, carb: 18, prot: 0, fat: 0 },
+  "coca cola": { name: "Coca Cola", cal: 140, carb: 35, prot: 0, fat: 0 },
+  "sprite": { name: "Sprite", cal: 130, carb: 33, prot: 0, fat: 0 },
+  "fanta": { name: "Fanta", cal: 150, carb: 38, prot: 0, fat: 0 },
+  "teh pucuk": { name: "Teh Pucuk", cal: 80, carb: 20, prot: 0, fat: 0 }
+};
